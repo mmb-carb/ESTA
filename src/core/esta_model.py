5fb57f56b2e78f1f54331c1898578e6a9fdbac03
theJollySin <thejollysin@gmail.com>
2017-03-29 08:36:20 -0700

    fixing spelling error

--- src/core/esta_model.py
+++ src/core/esta_model.py
@@ -5,11 +5,11 @@ from src.core.version import __version__
 
 class EstaModel(object):
 
-    def __init__(self, spatial_loaders, temporal_loaders, emis_loaders, emis_scalers, writers, tests):
+    def __init__(self, spatial_loaders, temporal_loaders, emis_loaders, emis_scalars, writers, tests):
         self.spatial_loaders = spatial_loaders
         self.temporal_loaders = temporal_loaders
         self.emis_loaders = emis_loaders
-        self.emis_scalers = emis_scalers
+        self.emis_scalars = emis_scalars
         self.writers = writers
         self.testers = tests
         self.spat_surrs = None
@@ -40,8 +40,8 @@ class EstaModel(object):
 
         print('  - scaling emissions & writing files')
         output_files = OutputFiles()
-        for scaler in self.emis_scalers:
-            for scaled_emissions in scaler.scale(self.emissions, self.spat_surrs, self.temp_surrs):
+        for scalar in self.emis_scalars:
+            for scaled_emissions in scalar.scale(self.emissions, self.spat_surrs, self.temp_surrs):
                 for writer in self.writers:
                     output_files.union(writer.write(scaled_emissions))
 
@@ -49,3 +49,4 @@ class EstaModel(object):
             print('  - testing output files')
         for tester in self.testers:
             tester.test(self.emissions, output_files)
+
