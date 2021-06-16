a077f07ea4b4fde566438f8b146c8363a2e2c465
dchau20 <57499119+dchau20@users.noreply.github.com>
2021-06-16 16:39:56 -0700

    Add files via upload

---
+++ NH3_data_EF17_MPO010/EF17_format_hd/cat_hd_nh3.sh
@@ -0,0 +1,14 @@
+path=../../output/emfac2017/v20210611/2018/hdv
+for month in {01..12}; do
+  export mth=$month
+
+  for dd in {01..31} ; do
+   export day=$dd
+   if [ -d $path/$mth/$day ]
+   then
+
+   cat nh3_hd_2018.csv >> $path/$mth/$day/emfac_hd.csv_all
+
+   fi
+  done
+done
